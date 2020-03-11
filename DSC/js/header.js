4e1484c1d7d073ca33910e9fa1a638215beaaf43
RyuYama <r_yamaguchi@dscnet.co.jp>
2020-03-12 08:25:01 +0900

    commit

---
+++ DSC/js/header.js
@@ -0,0 +1,20 @@
+html{
+}
+
+body{
+	font-family: 'Comic Sans MS', cursive;
+	margin:0;
+    background-color: #0f4c81;
+    color: white;
+}
+
+.FooterWrapper{
+	height:100px;
+    width: 100%;  
+}
+.Footer{
+    text-align: center;
+    margin: 40px;
+}
+
+
